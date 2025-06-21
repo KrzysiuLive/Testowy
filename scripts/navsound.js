ab5ce9d3fc6b277da57c162c9af98ec6484936c1
KrzysiuLive <krzysiooxdwa@gmail.com>
2025-06-21 22:55:38 +0200

    Delete scripts directory

--- scripts/navsound.js
+++
@@ -1,8 +0,0 @@
-
-const navSound = new Audio("assets/sounds/nav.mp3");
-document.querySelectorAll("a").forEach(el => {
-  el.addEventListener("click", () => {
-    navSound.currentTime = 0;
-    navSound.play();
-  });
-});
